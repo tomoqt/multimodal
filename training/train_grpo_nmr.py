668bfed596e370017de74efcf6bb0ca73b45173c
Tomoqt <francesco.pappone99@gmail.com>
2025-06-26 10:47:56 +0200

    new peft initialization

--- training/train_grpo_nmr.py
+++ training/train_grpo_nmr.py
@@ -269,10 +269,10 @@ def main():
             bias="none",
             task_type="CAUSAL_LM",
         )
-        model = AutoModelForCausalLM.from_pretrained(
-            args.model_name,
-            peft_config=peft_config,
-        )
+        model = AutoModelForCausalLM.from_pretrained(args.model_name)
+        from peft import get_perft_model
+        model = get_perft_model(model,peft_config)
+        lora_model.print_trainable_parameters()
     else:
         print("PEFT disabled. Using full fine-tuning.")
         model = AutoModelForCausalLM.from_pretrained(args.model_name)
